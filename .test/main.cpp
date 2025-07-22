ba018ba92a7949db5697f8e4851560da4f64f31b
Xtarii <carlalvinelias@gmail.com>
2025-07-22 20:28:37 +0200

    crawler test

--- .test/main.cpp
+++ .test/main.cpp
@@ -1,29 +1,38 @@
-#include "../headers/core.h"
-#include "../headers/URI/URI.h"
-#include "../headers/robots/Robots.h"
-#include "../headers/HTML/HTML.h"
-#include "../headers/algorithm/Algorithm.h"
-
-#include <set>
-#include <string>
+#include "../headers/crawler/Manager.h"
+#include "../headers/crawler/worker/Worker.h"
 
 
 
 int main() {
-    std::string agent = "*";
-    std::string url = "https://google.com/";
+    // std::string agent = "*";
 
+    std::string url = "https://google.com/";
     OSSE::URI URI = OSSE::URI::parse(url, nullptr);
 
-    OSSE::Robots robots = OSSE::Robots::load(&URI, nullptr);
+    // OSSE::Robots robots = OSSE::Robots::load(&URI, nullptr);
 
-    std::string page = OSSE::GET(URI);
-    OSSE::HTML::Document document(page);
+    // std::string page = OSSE::GET(URI);
+    // OSSE::HTML::Document document(page);
+
+
+    // std::set<std::string> tags =
+    //     OSSE::Algorithm::analyzeDocument(&document, config);
+
+
 
     CONFIG_LIST config = OSSE::Config::loadConfigList("./.config/tags.list");
 
-    std::set<std::string> tags =
-        OSSE::Algorithm::analyzeDocument(&document, config);
+    OSSE::Manager manager(config, nullptr);
+    manager.push(URI);
+
+
+    OSSE::Worker worker(&manager);
+
+
+    OSSE::Robots robots = OSSE::Robots::load(&URI, nullptr);
+    OSSE::Manager::QueueObject obj(URI, robots);
+    worker.run(&obj);
+
 
 
     return 0;
