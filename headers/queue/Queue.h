45de2b9c3021bb85ddaa2f54bd0a3e27b039583b
Xtarii <carlalvinelias@gmail.com>
2025-07-22 20:29:03 +0200

    Created a thread safe queue

---
+++ headers/queue/Queue.h
@@ -0,0 +1,66 @@
+#ifndef OSSE_QUEUE
+#define OSSE_QUEUE
+
+#include <mutex>
+#include <queue>
+
+namespace OSSE {
+    /**
+     * Thread safe Queue
+     *
+     * @tparam T Type of object to store
+     */
+    template<typename T>
+    struct Queue {
+        private:
+            /**
+             * Mutex object
+             */
+            std::mutex mutex_;
+
+            /**
+             * Queue object
+             */
+            std::queue<T> queue_;
+
+
+
+        public:
+            /**
+             * Pushes object back in queue
+             *
+             * @param obj Object to push back
+             */
+            void push(T obj) {
+                std::unique_lock<std::mutex> lock(mutex_);
+                queue_.push(obj);
+            }
+
+            /**
+             * Pops object from queue
+             *
+             * @return Object popped
+             */
+            T pop() {
+                std::unique_lock<std::mutex> lock(mutex_);
+                T obj = queue_.front();
+                queue_.pop();
+                return obj;
+            }
+
+
+
+            /**
+             * Checks if queue is empty
+             *
+             * @return Queue is empty
+             */
+            bool empty() {
+                std::unique_lock<std::mutex> lock(mutex_);
+                return queue_.empty();
+            }
+
+    };
+}
+
+#endif // OSSE_QUEUE
