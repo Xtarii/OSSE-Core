88411a758b6f2b1f902937e70289e761c9baf997
Xtarii <carlalvinelias@gmail.com>
2025-07-22 15:40:07 +0200

    updated test for analyzing

--- .test/main.cpp
+++ .test/main.cpp
@@ -2,7 +2,9 @@
 #include "../headers/URI/URI.h"
 #include "../headers/robots/Robots.h"
 #include "../headers/HTML/HTML.h"
+#include "../headers/algorithm/Algorithm.h"
 
+#include <set>
 #include <string>
 
 
@@ -18,6 +20,10 @@ int main() {
     std::string page = OSSE::GET(URI);
     OSSE::HTML::Document document(page);
 
+    CONFIG_LIST config = OSSE::Config::loadConfigList("./.config/tags.list");
+
+    std::set<std::string> tags =
+        OSSE::Algorithm::analyzeDocument(&document, config);
 
 
     return 0;
