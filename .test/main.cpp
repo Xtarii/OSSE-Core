3eea05926c5edb067bcb2b534c89d216aa78c60f
Xtarii <carlalvinelias@gmail.com>
2025-08-01 23:50:56 +0200

    simple terminal test search

--- .test/main.cpp
+++ .test/main.cpp
@@ -1,27 +1,13 @@
 #include "../headers/crawler/Manager.h"
-// #include "../headers/crawler/worker/Worker.h"
-// #include "../headers/algorithm/Algorithm.h"
+#include "../headers/algorithm/Algorithm.h"
 #include <iostream>
-// #include <set>
 #include <string>
 
 
 
 int main() {
-    // std::string agent = "*";
-
     std::string url = "https://google.com/";
     OSSE::URI URI = OSSE::URI::parse(url, nullptr);
-
-    // OSSE::Robots robots = OSSE::Robots::load(&URI, nullptr);
-
-    // std::string page = OSSE::GET(URI);
-    // OSSE::HTML::Document document(page);
-
-
-    // std::set<std::string> tags =
-    //     OSSE::Algorithm::analyzeDocument(&document, config);
-
 
 
     CONFIG_LIST config = OSSE::Config::loadConfigList("./.config/tags.list");
@@ -33,29 +19,23 @@ int main() {
     manager.run();
 
 
-    std::cout << "Q to quit search" << std::endl;
+    std::cout << "\"q\" to quit search" << std::endl;
     while(true) {
         std::string x;
         std::cin >> x;
-        if(x.compare("q") == 0) break;
+        if(x.compare("q") == 0) {
+            break;
+            return 0;
+        }
+
+        manager.run();
+        std::set<std::string> tags = OSSE::Algorithm::analyzeText(x, config);
+        std::set<OSSE::Database::object*> result = manager.database()->get(tags);
+        for(OSSE::Database::object* page : result) {
+            std::cout << "Found: " << page->title << ", at " << page->URI.fullURI() << std::endl;
+        }
+        std::cout << std::endl;
     }
-
-
-    // OSSE::Worker worker(&manager);
-
-
-    // OSSE::Robots* robots = OSSE::Robots::load(&URI, nullptr);
-    // OSSE::Manager::QueueObject obj(URI, robots);
-    // worker.run(&obj);
-
-
-    // std::set<std::string> tags = OSSE::Algorithm::analyzeText(
-    //     "google", config
-    // );
-    // std::set<OSSE::Database::object*> pages = manager.database()->get(tags);
-    // for(OSSE::Database::object* page : pages) {
-    //     std::cout << "Found: " << page->title << ", at " << page->URI.fullURI() << std::endl;
-    // }
 
 
     return 0;
