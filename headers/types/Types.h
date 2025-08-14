a9e6e8e41f526f0c69b0aaf7859e50b0108ad1da
Xtarii <carlalvinelias@gmail.com>
2025-08-14 21:57:05 +0200

    Moved Queue type to Types

--- headers/types/Types.h
+++ headers/types/Types.h
@@ -1,12 +1,37 @@
 #ifndef OSSE_TYPES
 #define OSSE_TYPES
 
+#include "../URI/URI.h"
+#include "../robots/Robots.h"
+
 #include <set>
 #include <string>
 #include <vector>
 
 namespace OSSE {
     struct database_object;
+
+    /// Manager URI Queue object
+    struct uri_object {
+        /// URI Object
+        OSSE::URI URI;
+        /// Robots Object
+        OSSE::Robots* Robots;
+
+        /**
+         * Construct a new URI Queue object
+         *
+         * @param uri URI object
+         * @param robots Robots object
+         */
+        uri_object(OSSE::URI uri, OSSE::Robots* robots) : URI(uri), Robots(robots) {
+            OSSE::Robots::subscribe(this->Robots);
+        }
+
+        ~uri_object() {
+            OSSE::Robots::unsubscribe(this->Robots);
+        }
+    };
 
 
 
